302f76b21fc72516822e855a357321ea3ed4466e
Joanerson Costa <joanersonc31@gmail.com>
2023-11-08 13:03:46 -0300

    implementa dominio pedido

---
+++ src/main/java/br/com/siteware/conteudo/pedido/application/api/EnderecoAPI.java
@@ -0,0 +1,9 @@
+package br.com.siteware.conteudo.pedido.application.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/cliente/{idCliente}")
+public interface EnderecoAPI {
+}
---
+++ src/main/java/br/com/siteware/conteudo/pedido/domain/Pedido.java
@@ -0,0 +1,30 @@
+package br.com.siteware.conteudo.pedido.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "idPedido", updatable = false, unique = true, nullable = false)
+	private UUID idPedido;
+	@NotNull
+	@Column(columnDefinition = "uuid", name = "idCliente", nullable = false)
+	private UUID idCliente;
+
+	private LocalDateTime momentoDoPedido;
+
+}
