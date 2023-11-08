1f10f22f9047f30b6d3b92abbfa3639b7e0bbb44
Joanerson Costa <joanersonc31@gmail.com>
2023-11-08 11:16:45 -0300

    configura handler no projeto

--- src/main/java/br/com/siteware/conteudo/cliente/application/repository/ClientRepository.java
+++ src/main/java/br/com/siteware/conteudo/cliente/application/repository/ClientRepository.java
@@ -1,6 +1,8 @@
 package br.com.siteware.conteudo.cliente.application.repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import br.com.siteware.conteudo.cliente.domain.Cliente;
 
