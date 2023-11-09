7acd6869d01870340dbd3e08fadc786d8c4721eb
Joanerson Costa <joanersonc31@gmail.com>
2023-11-09 19:47:57 -0300

    implementa contra busca produto por id

--- src/main/java/br/com/siteware/conteudo/produto/application/api/ProdutoAPI.java
+++ src/main/java/br/com/siteware/conteudo/produto/application/api/ProdutoAPI.java
@@ -3,6 +3,7 @@ package br.com.siteware.conteudo.produto.application.api;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.siteware.conteudo.pedido.application.api.PedidoDetalhadoResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -19,5 +21,9 @@ public interface ProdutoAPI {
 	@PostMapping(path = "/produto")
 	@ResponseStatus(value = HttpStatus.CREATED)
 	ProdutoIdResponse postProduto(@PathVariable(value = "idCliente") UUID idCliente, @PathVariable(value = "idPedido") UUID idPedido, @RequestBody @Valid ProdutoRequest produtoRequest);
+
+	@GetMapping(path = "/produto/{idProduto}")
+	@ResponseStatus(value = HttpStatus.OK)
+	ProdutoDetalhadoResponse buscaProdutoPorId(@PathVariable(value = "idCliente") UUID idCliente, @PathVariable(value = "idPedido") UUID idPedido, @PathVariable(value = "idProduto") UUID idProduto);
 
 }
