eecf4542291f072e8127b2fa768d9d4d59b73e38
Joanerson Costa <joanersonc31@gmail.com>
2023-11-10 06:34:39 -0300

    remove idProduto do dominio

--- src/main/java/br/com/siteware/conteudo/categoria/domain/Categoria.java
+++ src/main/java/br/com/siteware/conteudo/categoria/domain/Categoria.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,16 +17,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 public class Categoria {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(columnDefinition = "uuid", name = "idCategoria", updatable = false, unique = true, nullable = false)
 	private UUID idCategoria;
-	@NotNull
-	@Column(columnDefinition = "uuid", name = "idProduto", nullable = false)
-	private UUID idProduto;
 	@NotBlank
 	@Size(message = "Campo nome da categoria não pode estar vazio", max = 50, min = 3)
 	private String nome;
-	
 	
 }
