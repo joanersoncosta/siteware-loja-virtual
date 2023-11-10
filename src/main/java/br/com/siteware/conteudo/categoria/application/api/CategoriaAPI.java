8099e0263f65a3dda4ea989673a96f4568fcd54c
Joanerson Costa <joanersonc31@gmail.com>
2023-11-10 07:17:34 -0300

    implementa contrato lista categorias no API

--- src/main/java/br/com/siteware/conteudo/categoria/application/api/CategoriaAPI.java
+++ src/main/java/br/com/siteware/conteudo/categoria/application/api/CategoriaAPI.java
@@ -1,12 +1,16 @@
 package br.com.siteware.conteudo.categoria.application.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.siteware.conteudo.produto.application.api.ProdutoPedidoListResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -15,5 +19,9 @@ public interface CategoriaAPI {
 	@PostMapping
 	@ResponseStatus(value = HttpStatus.CREATED)
 	CategoriaIdResponse postCategoria(@RequestBody @Valid CategoriaRequest CategoriaRequest);
+
+	@GetMapping
+	@ResponseStatus(value = HttpStatus.OK)
+	List<CategoriaListResponse> buscaTodasCategorias();
 
 }
--- src/main/java/br/com/siteware/conteudo/categoria/application/api/CategoriaRestController.java
+++ src/main/java/br/com/siteware/conteudo/categoria/application/api/CategoriaRestController.java
@@ -1,5 +1,7 @@
 package br.com.siteware.conteudo.categoria.application.api;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.siteware.conteudo.categoria.application.service.CategoriaService;
@@ -18,6 +20,13 @@ public class CategoriaRestController implements CategoriaAPI {
 		CategoriaIdResponse categoriaIdResponse = CategoriaService.salvaCategoria(CategoriaRequest);
 		log.info("[finaliza] CategoriaRestController - postCategoria");
 		return categoriaIdResponse;
+	}
+
+	@Override
+	public List<CategoriaListResponse> buscaTodasCategorias() {
+		log.info("[inicia] CategoriaRestController - buscaTodasCategorias");
+		log.info("[finaliza] CategoriaRestController - buscaTodasCategorias");
+		return null;
 	}
 
 }
