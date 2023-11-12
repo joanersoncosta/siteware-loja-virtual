b9a23f269acdac259d84d893990126269391f671
Joanerson Costa <joanersonc31@gmail.com>
2023-11-12 08:22:38 -0300

    modifica mapeamento do post produto para request param

--- src/main/java/br/com/siteware/conteudo/produto/application/api/ProdutoAPI.java
+++ src/main/java/br/com/siteware/conteudo/produto/application/api/ProdutoAPI.java
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.siteware.conteudo.pedido.application.api.PedidoAlteracaoRequest;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/produto")
 public interface ProdutoAPI {
 	
-	@PostMapping(path = "/categoria/{idCategoria}")
+	@PostMapping(path = "/post")
 	@ResponseStatus(value = HttpStatus.CREATED)
-	ProdutoIdResponse postProduto(@PathVariable(value = "idCategoria") UUID idCategoria, @RequestBody @Valid ProdutoRequest produtoRequest);
+	ProdutoIdResponse postProduto(@RequestParam(value = "idCategoria") UUID idCategoria, @RequestBody @Valid ProdutoRequest produtoRequest);
 
 	@GetMapping(path = "/{idProduto}")
 	@ResponseStatus(value = HttpStatus.OK)
